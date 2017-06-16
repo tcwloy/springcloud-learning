6cbf066116aaf82c241d602499e936972a86944b
tcwloy <tcwloy08@163.com>
2017-06-16 16:44:41 +0800

    初始化配置中心、注册中心和网关

---
+++ config-center/src/main/java/com/tcwloy/cloud/ConfigCenterApplication.java
@@ -0,0 +1,12 @@
+package com.tcwloy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenterApplication.class, args);
+	}
+}
